b9bf69dec0d416a7f58ad37f7016f42d2fdee3d9
Marcel Straub <m@straubs.eu>
2022-10-26 11:31:07 +0200

    Implemented __enter__ and __exit__ methods for database accessor

--- accessors/databaseAccessor.py
+++ accessors/databaseAccessor.py
@@ -1,6 +1,24 @@
 from abc import ABC, abstractmethod
+import logging
 
 class DatabaseAccessor(ABC):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def __enter__(self):
+        logging.debug("Executing DatabaseAccessor::__enter__()")
+        self.open()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        logging.debug("Executing DatabaseAccessor::__exit__()")
+        self.close()
+
     @abstractmethod
     def insertMeasurement(self, timestamp: int, sensorName: str, sensorValue: float):
         pass
@@ -8,5 +26,4 @@ class DatabaseAccessor(ABC):
     @abstractmethod
     def getLastDeviceTimestamp(self):
         pass
-
     
--- accessors/postgresDatabaseAccessor.py
+++ accessors/postgresDatabaseAccessor.py
@@ -6,24 +6,50 @@ import psycopg2
 import logging
 
 class PostgresDatabaseAccessor(DatabaseAccessor):
+    # Class infrastructure
     def __init__(self, configuration: SqlConfiguration):
         self._config = configuration
+        self._connection = None
+        self._cursor = None
+        self.open()
 
-        self._connection = psycopg2.connect(
-            host=self._config.host,
-            database=self._config.database,
-            user=self._config.user,
-            password=self._config.password
-            )
-        self._connection.set_client_encoding('UTF8')
-        self._cursor = self._connection.cursor()
+    def open(self):
+        if not self.isConnectionOpen():
+            logging.debug("Opening SQL connection")
+            self._connection = psycopg2.connect(
+                host=self._config.host,
+                database=self._config.database,
+                user=self._config.user,
+                password=self._config.password
+                )
+            self._connection.set_client_encoding('UTF8')
+            self._cursor = self._connection.cursor()
 
+    def close(self):
+        if self.isConnectionOpen():
+            logging.debug("Closing SQL connection")
+            self._connection.commit()
+            self._connection.close()
+
+            self._connection = None
+            self._cursor = None
+
+    def isConnectionOpen(self) -> bool:
+        if self._connection is None:
+            return False
+        elif self._connection.closed != 0:
+            return False
+        else:
+            return True
+
+    ## Interfaces to database
     def insertMeasurement(self, timestamp: int, sensorName: str, sensorValue: float):
         logging.debug("CALL \"data\".\"pInsertMeasurement\"(%s,%s,%s,%s);" % (timestamp,self._config.deviceName,sensorName,sensorValue,))
         self._cursor.execute(
             "CALL \"data\".\"pInsertMeasurement\"(%s,%s,%s,%s);"
             ,(timestamp,self._config.deviceName,sensorName,sensorValue,)
         )
+        self._connection.commit()
 
     def getLastDeviceTimestamp(self):
         logging.debug("SELECT * FROM \"data\".\"fnGetLatestMeasurementOfDevice\"('{}');'".format(self._config.deviceName))
