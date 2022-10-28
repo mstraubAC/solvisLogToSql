8b48cfd7e0f3dd82d37d8d366ed33990a2828d3d
Marcel Straub <m@straubs.eu>
2022-10-28 19:50:14 +0200

    enhance type hinting in configuration model

--- model/configuration.py
+++ model/configuration.py
@@ -1,7 +1,7 @@
 import json
 
 class SolvisConfiguration:
-    def __init__(self, host: str, port: str, user: str, password: str, cachedir: str, alwaysFetchLastTwoMeasurementFiles: bool, logFileEncoding: str):
+    def __init__(self, host: str, port: str, user: str, password: str, cachedir: str, alwaysFetchLastTwoMeasurementFiles: bool, logFileEncoding: str) -> None:
         self.host = host
         self.port = port
         self.user = user
@@ -10,17 +10,17 @@ class SolvisConfiguration:
         self.alwaysFetchLastTwoMeasurementFiles = alwaysFetchLastTwoMeasurementFiles
         self.logFileEncoding = logFileEncoding
 
-    def __str__(self):
+    def __str__(self) -> str():
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def toJson(self):
+    def toJson(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
 
     @staticmethod
-    def fromJson(json: dict):
+    def fromJson(json: dict[str, any]) -> 'SolvisConfiguration':
         # default value initializations
         alwaysFetchLastTwoMeasurementFiles = json['alwaysFetchLastTwoMeasurementFiles'] if 'alwaysFetchLastTwoMeasurementFiles' in json else True        
         logFileEncoding = json['logFileEncoding'] if 'logFileEncoding' in json else 'ISO-8859-1'
@@ -35,24 +35,24 @@ class SolvisConfiguration:
             )
 
 class SqlConfiguration:
-    def __init__(self, host: str, user: str, password: str, database: str, deviceName: str):
+    def __init__(self, host: str, user: str, password: str, database: str, deviceName: str) -> None:
         self.host = host
         self.user = user
         self.password = password
         self.database = database
         self.deviceName = deviceName
 
-    def __str__(self):
+    def __str__(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def toJson(self):
+    def toJson(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
 
     @staticmethod
-    def fromJson(json: dict):
+    def fromJson(json: dict[str, any]) -> 'SqlConfiguration':
         return SqlConfiguration(
             json['host'],
             json['user'],
@@ -63,21 +63,21 @@ class SqlConfiguration:
 
 
 class Configuration:
-    def __init__(self, solvis: SolvisConfiguration, sql: SqlConfiguration):
+    def __init__(self, solvis: SolvisConfiguration, sql: SqlConfiguration) -> None:
         self.solvis = solvis
         self.sql = sql
 
-    def __str__(self):
+    def __str__(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
     
-    def toJson(self):
+    def toJson(self) -> str:
         return json.dumps(dict(self), ensure_ascii=False)
 
     @staticmethod
-    def fromJson(solvis, sql):
+    def fromJson(solvis: dict[str, any], sql: dict[str, any]) -> 'Configuration':
         return Configuration(
             SolvisConfiguration.fromJson(solvis),
             SqlConfiguration.fromJson(sql)
